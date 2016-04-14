3d24b99f496ad3707fb3fc6bc5136b44cf4a3a97
Yohan Gomez <yohan.gz@hotmail.com>
2016-04-14 23:16:42 +0530

    Update karma coverage file comments.

--- generators/karma-conf/templates/karma-coverage.conf.js
+++ generators/karma-conf/templates/karma-coverage.conf.js
@@ -3,7 +3,11 @@
 module.exports = function(config) {
 
   config.set({
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['<%= testFramework %>', 'browserify'],
+
+    // list of files / patterns to load in the browser
     files: [
       'src/**/*.js',
       'test/**/*.js',
@@ -12,23 +16,38 @@ module.exports = function(config) {
       { pattern: 'src/**/*.js.map', included: false, watched: true },
       { pattern: 'test/**/*.js.map', included: false, watched: true }
     ],
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
       'src/**/*.js': ['browserify'],
       'test/**/*.js': ['browserify']
     },
+
+
     browserify: {
       transform: ['browserify-istanbul']
     },
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: [
       'Chrome'
     ],
-    reporters: ['progress', 'coverage'],
-      coverageReporter: {
-          type: 'json',
-          subdir: '.',
-          file: 'coverage-final.json'
-      },
-    singleRun: true
 
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress', 'coverage'],
+
+    coverageReporter: {
+        type: 'json',
+        subdir: '.',
+        file: 'coverage-final.json'
+    },
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: true
   });
 };
