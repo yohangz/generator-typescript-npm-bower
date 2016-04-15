58975b362abdef21971adab87f9c3cf2f624e341
Yohan Gomez <yohan.gz@hotmail.com>
2016-04-15 12:19:51 +0530

    Update bower-conf unit tests

--- test/bower-conf/index.js
+++ test/bower-conf/index.js
@@ -2,6 +2,7 @@
 var path = require('path');
 var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
+var fs = require('fs');
 
 describe('generator-npm-bower-module:bower-conf', function () {
     before(function (done) {
@@ -9,9 +10,18 @@ describe('generator-npm-bower-module:bower-conf', function () {
             .on('end', done);
     });
 
-    it('creates .bowerrc file', function () {
-        assert.file([
-            '.bowerrc'
+    it('creates .bowerrc file with correct content', function (done) {
+      fs.readFile(path.join(__dirname, 'templates/bowerrc-template'), 'utf8', function (err, data) {
+        if (err) {
+          console.log(err);
+          done(err)
+        }
+
+        assert.fileContent([
+          ['.bowerrc', data]
         ]);
+
+        done()
+      });
     });
 });
