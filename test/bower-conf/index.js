d2aaba98fac3b9cf09376f341c8ff8a41bd9c609
Yohan Gomez <yohan.gz@hotmail.com>
2016-04-15 12:23:19 +0530

    Update editor-conf unit tests

--- test/bower-conf/index.js
+++ test/bower-conf/index.js
@@ -5,23 +5,23 @@ var helpers = require('yeoman-test');
 var fs = require('fs');
 
 describe('generator-npm-bower-module:bower-conf', function () {
-    before(function (done) {
-        helpers.run(path.join(__dirname, '../../generators/bower-conf'))
-            .on('end', done);
+  before(function (done) {
+      helpers.run(path.join(__dirname, '../../generators/bower-conf'))
+          .on('end', done);
+  });
+
+  it('creates .bowerrc file with correct content', function (done) {
+    fs.readFile(path.join(__dirname, 'templates/bowerrc-template'), 'utf8', function (err, data) {
+      if (err) {
+        console.log(err);
+        done(err)
+      }
+
+      assert.fileContent([
+        ['.bowerrc', data]
+      ]);
+
+      done()
     });
-
-    it('creates .bowerrc file with correct content', function (done) {
-      fs.readFile(path.join(__dirname, 'templates/bowerrc-template'), 'utf8', function (err, data) {
-        if (err) {
-          console.log(err);
-          done(err)
-        }
-
-        assert.fileContent([
-          ['.bowerrc', data]
-        ]);
-
-        done()
-      });
-    });
+  });
 });
